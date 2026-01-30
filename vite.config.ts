e1721e6db22b79c7753c3cad3e1f7a4cbf728139
v_axhuili <v_axhuili@tencent.com>
2026-01-30 11:13:06 +0800

    chore: 更新 Vite 基础路径以适配 GitHub Pages 子目录部署
    将基础路径从 './' 更改为 './clara-test/'，确保应用在 GitHub Pages 的特定子目录下能正确加载静态资源。

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./", // 设为相对路径，适配 GitHub Pages 非根目录部署
+  base: "./clara-test/", // 设为相对路径，适配 GitHub Pages 非根目录部署
   plugins: [vue()],
 });
